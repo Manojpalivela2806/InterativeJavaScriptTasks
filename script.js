ed0c53ac370993e556d45908cff16d72821afb8f
ManojPalivela <122717494+Manojpalivela2806@users.noreply.github.com>
2024-03-02 00:49:19 +0530

    simple OTP validation  by JS

--- script.js
+++ script.js
@@ -1,25 +1,16 @@
-let inp = document.getElementById("one");
-let form = document.getElementById("form");
-let con = document.getElementById("con");
-let check = document.createElement("h2");
-document.body.appendChild(check);
-
-
-// console.log(rand);
-let weather=["foggy","thunderStrom","rainy","partialy cloud","cloudy","sunny Day"];
-let randomWeather = weather[Math.floor(Math.random() * weather.length)];
-
-form.addEventListener("submit",(e)=>{
-    let rand = Math.floor(Math.random()*50);
-    e.preventDefault();
-    if(inp.value!=""){   
-    let city = document.getElementById("one");
-    console.log(city.value);
-    con.innerHTML="Current Temperature in "+city.value+" is "+rand+" \u00B0C "+randomWeather
-    check.textContent="";
-    }
-    else{
-        check.textContent="Please provide the city name";
-        check.style.color="red";
-    }
+let e=document.getElementById("bu");
+console.log(e);
+var bd=document.getElementById("body")
+console.log(bd);
+e.addEventListener("click",()=>{
+    var red = Math.round(Math.random()*255);
+    var green = Math.round(Math.random()*125);
+    var blue = Math.round(Math.random()*255);
+    document.body.style.backgroundColor="rgb("+red+","+green+","+blue+")";
 })
+// function run(){
+//     var red = Math.round(Math.random()*255);
+//     var green = Math.round(Math.random()*125);
+//     var blue = Math.round(Math.random()*255);
+//     document.body.style.backgroundColor="rgb("+red+","+red+","+red+")";   
+// }
